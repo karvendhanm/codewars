022db158784809521adb24d3ba696f09f42ee6cb
Karvendhan <karvsmech@gmail.com>
2020-03-07 06:19:26 +0530

    tic_tac_toe my solution improved

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -13,22 +13,20 @@ def is_solved(board):
 def is_solved(board):
     lst = np.array(board)
     _all_dim = np.concatenate((lst, lst.transpose(), [np.diagonal(lst)], [np.fliplr(lst).diagonal()]), axis=0)
-    unfinished_game = 0
 
     for row in range(_all_dim.shape[0]):
         elem = set(_all_dim[row, :])
-        if {0}.intersection(elem): unfinished_game = 1
         if len(elem) == 1 and list(elem)[0] != 0:
             return list(elem)[0]
 
-    if unfinished_game:
+    if 0 in lst.ravel():
         return -1
 
     return 0
 
-lst = [[2, 1, 2],
-       [0, 1, 2],
-       [2, 1, 0]]
+lst = [[2, 1, 1],
+       [1, 1, 0],
+       [2, 1, 2]]
 
 is_solved(lst)
 
